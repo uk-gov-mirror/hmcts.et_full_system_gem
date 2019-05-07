a3342d70e525867f662895c4609af8d80b56cf4f
Gary Taylor <gary.taylor@hismessages.com>
2019-05-07 15:43:39 +0100

    --without now working for docker server

--- lib/et_full_system/cli/docker/server.rb
+++ lib/et_full_system/cli/docker/server.rb
@@ -7,10 +7,14 @@ module EtFullSystem
       class ServerCommand < Thor
         BEFORE_BOOT_SCRIPT =
         desc "up", "Starts the full system server on docker"
+        method_option :without, type: :array, default: [], banner: "service1 service2", desc: "If specified, disables the specified services from running. The services are et1_web, et1_sidekiq, et3_web, mail_web, api_web, api_sidekiq, admin_web, atos_api_web, s3_web, azure_blob_web, fake_acas_web"
         def up(*args)
           Bundler.with_original_env do
+            server_args = []
+            server_args << "--without=#{options[:without].join(' ')}" unless options[:without].empty?
+            env_vars = "SERVER_ARGS='#{server_args.join(' ')}'"
             gem_root = File.absolute_path('../../../..', __dir__)
-            cmd = "docker-compose -f #{gem_root}/docker/docker-compose.yml up #{args.join(' ')}"
+            cmd = "#{env_vars} docker-compose -f #{gem_root}/docker/docker-compose.yml up #{args.join(' ')}"
             puts cmd
             exec(cmd)
           end
