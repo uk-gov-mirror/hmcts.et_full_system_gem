ed195910da634f3a616942fb923c50f89cd4a265
Gary Taylor <gary.taylor@hismessages.com>
2019-07-08 08:50:39 +0100

    pull and checkout added to workspace

--- lib/et_full_system/cli/workspace.rb
+++ lib/et_full_system/cli/workspace.rb
@@ -12,5 +12,18 @@ module EtFullSystem
       return say "The current directory must be empty" unless Dir.empty?(Dir.pwd)
       puts `git clone --recursive git@github.com:hmcts/et-full-system-servers.git .`
     end
+
+    desc "pull", "Pulls the latest version from the repository and updates the submodules"
+    def pull
+      return say "Please change to a workspace setup by this tool" unless File.exist?(File.join(Dir.pwd, 'et-full-system.dir'))
+      puts `git pull && git submodule update`
+    end
+
+    desc "checkout <branch-or-commit>", "Checkout a branch or a specific commit, then updates the submodules"
+    def checkout(branch_or_commit)
+      return say "Please change to a workspace setup by this tool" unless File.exist?(File.join(Dir.pwd, 'et-full-system.dir'))
+      puts `git checkout #{branch_or_commit}`
+      puts `git submodule update`
+    end
   end
 end
--- lib/et_full_system/version.rb
+++ lib/et_full_system/version.rb
@@ -1,3 +1,3 @@
 module EtFullSystem
-  VERSION = "0.1.55"
+  VERSION = "0.1.56"
 end
