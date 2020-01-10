8852f80bb55177984774841ec105e1a990b44e5c
Gary Taylor <gary.taylor@hismessages.com>
2020-01-10 11:02:00 +0000

    Added ability to use localhost.from.docker as a platform independent way of getting back to the host

--- lib/et_full_system/cli/docker.rb
+++ lib/et_full_system/cli/docker.rb
@@ -22,7 +22,7 @@ module EtFullSystem
       Bundler.with_original_env do
         gem_root = File.absolute_path('../../..', __dir__)
         cmd = "/bin/bash --login -c \"cd /home/app/full_system && et_full_system docker bootstrap && et_full_system local setup\""
-        compose_cmd = "docker-compose -f #{gem_root}/docker/docker-compose.yml run --rm et #{cmd}"
+        compose_cmd = "GEM_VERSION=#{EtFullSystem::VERSION} LOCALHOST_FROM_DOCKER_IP=#{host_ip} docker-compose -f #{gem_root}/docker/docker-compose.yml run --rm et #{cmd}"
         puts compose_cmd
         exec(compose_cmd)
       end
@@ -32,7 +32,7 @@ module EtFullSystem
     def compose(*args)
       Bundler.with_original_env do
         gem_root = File.absolute_path('../../..', __dir__)
-        cmd = "docker-compose -f #{gem_root}/docker/docker-compose.yml #{args.join(' ')}"
+        cmd = "GEM_VERSION=#{EtFullSystem::VERSION} LOCALHOST_FROM_DOCKER_IP=#{host_ip} docker-compose -f #{gem_root}/docker/docker-compose.yml #{args.join(' ')}"
         puts cmd
         exec(cmd)
       end
@@ -42,10 +42,10 @@ module EtFullSystem
     def reset
       Bundler.with_original_env do
         gem_root = File.absolute_path('../../..', __dir__)
-        cmd = "docker-compose -f #{gem_root}/docker/docker-compose.yml down -v"
+        cmd = "GEM_VERSION=#{EtFullSystem::VERSION} LOCALHOST_FROM_DOCKER_IP=#{host_ip} docker-compose -f #{gem_root}/docker/docker-compose.yml down -v"
         puts cmd
         next unless system(cmd)
-        cmd = "docker-compose -f #{gem_root}/docker/docker-compose.yml build --no-cache"
+        cmd = "GEM_VERSION=#{EtFullSystem::VERSION} LOCALHOST_FROM_DOCKER_IP=#{host_ip} docker-compose -f #{gem_root}/docker/docker-compose.yml build --no-cache"
         puts cmd
         next unless system(cmd)
         self.class.start(['setup'])
@@ -57,7 +57,7 @@ module EtFullSystem
       Bundler.with_original_env do
         gem_root = File.absolute_path('../../..', __dir__)
         cmd = "/bin/bash --login -c \"et_full_system local update_service_url #{service} #{url}\""
-        compose_cmd = "docker-compose -f #{gem_root}/docker/docker-compose.yml exec et #{cmd}"
+        compose_cmd = "GEM_VERSION=#{EtFullSystem::VERSION} LOCALHOST_FROM_DOCKER_IP=#{host_ip} docker-compose -f #{gem_root}/docker/docker-compose.yml exec et #{cmd}"
         puts compose_cmd
         exec(compose_cmd)
       end
@@ -73,13 +73,25 @@ module EtFullSystem
       Bundler.with_original_env do
         gem_root = File.absolute_path('../../..', __dir__)
         cmd = "/bin/bash --login -c \"et_full_system local service_env #{service}\""
-        compose_cmd = "docker-compose -f #{gem_root}/docker/docker-compose.yml exec et #{cmd}"
-        puts compose_cmd
-        exec(compose_cmd)
+        compose_cmd = "GEM_VERSION=#{EtFullSystem::VERSION} LOCALHOST_FROM_DOCKER_IP=#{host_ip} docker-compose -f #{gem_root}/docker/docker-compose.yml exec et #{cmd}"
+        result = `#{compose_cmd}`
+        replace_db_host_port(result)
+        replace_redis_host_port(result)
+        replace_smtp_host_port(result)
+        puts result
       end
     end
 
     private
+
+    def run_compose_command(*args, silent: false)
+      Bundler.with_original_env do
+        gem_root = File.absolute_path('../../..', __dir__)
+        cmd = "GEM_VERSION=#{EtFullSystem::VERSION} LOCALHOST_FROM_DOCKER_IP=#{host_ip} docker-compose -f #{gem_root}/docker/docker-compose.yml #{args.join(' ')}"
+        puts cmd unless silent
+        `#{cmd}`
+      end
+    end
 
     def host_ip
       result = JSON.parse `docker network inspect \`docker network list | grep docker_et_full_system | awk '{print $1}'\``
--- lib/et_full_system/cli/docker/server.rb
+++ lib/et_full_system/cli/docker/server.rb
@@ -10,11 +10,16 @@ module EtFullSystem
         method_option :without, type: :array, default: [], banner: "service1 service2", desc: "If specified, disables the specified services from running. The services are et1_web, et1_sidekiq, et3_web, mail_web, api_web, api_sidekiq, admin_web, atos_api_web, s3_web, azure_blob_web, fake_acas_web, fake_ccd_web"
         method_option :ccd_docker, type: :boolean, default: false, aliases: 'ccd-docker', desc: "If specified, instead of using the built in fake ccd server, the system will connect to your local machine (see ccd-docker-host option also)"
         method_option :ccd_docker_host, type: :string, default: 'docker.for.mac.localhost', aliases: 'ccd-docker-host', desc: "Only used if ccd-docker=true.  This specifies the host name of your machine when viewed from inside the docker container.  This defaults to docker.for.mac.localhost which is suitable for mac OSX only.  Consult docker documentation for other systems"
+        method_option :with_test, type: :boolean, default: false, aliases: 'with-test'
+        method_option :use_selenium, type: :boolean, default: true, aliases: 'use-selenium', desc: 'Only used if with_test is true - says to use selenium in preference to zalenium'
+        method_option :chrome_instances, type: :numeric, default: 1, aliases: 'chrome-instances', desc: 'Specify the number of chrome instances for selenium'
+        method_option :firefox_instances, type: :numeric, default: 1, aliases: 'firefox-instances', desc: 'Specify the number of firefox instances for selenium'
         def up(*args)
           Bundler.with_original_env do
             server_args = []
             server_args << "--without=#{options[:without].join(' ')}" unless options[:without].empty?
             env_vars = ["SERVER_ARGS='#{server_args.join(' ')}'"]
+            env_vars << "LOCALHOST_FROM_DOCKER_IP=#{host_ip}"
             if options.ccd_docker?
               env_vars << "CCD_AUTH_BASE_URL=http://#{options.ccd_docker_host}:4502"
               env_vars << "CCD_IDAM_BASE_URL=http://#{options.ccd_docker_host}:5000"
@@ -38,13 +43,20 @@ module EtFullSystem
         def down(*args)
           ::Bundler.with_original_env do
             gem_root = File.absolute_path('../../../..', __dir__)
-            cmd = "docker-compose -f #{gem_root}/docker/docker-compose.yml down #{args.join(' ')}"
+            cmd = "GEM_VERSION=#{EtFullSystem::VERSION} LOCALHOST_FROM_DOCKER_IP=#{host_ip} docker-compose -f #{gem_root}/docker/docker-compose.yml down #{args.join(' ')}"
             puts cmd
             exec(cmd)
           end
         end
 
         default_task :up
+
+        private
+
+        def host_ip
+          result = JSON.parse `docker network inspect \`docker network list | grep docker_et_full_system | awk '{print $1}'\``
+          result.first.dig('IPAM', 'Config').first['Gateway']
+        end
       end
     end
   end
