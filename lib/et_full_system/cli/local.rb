b6b218271dbda3e48093fbd679225bb1f27e232a
garytaylor <gary.taylor@hismessages.com>
2023-02-03 07:46:53 +0000

    Bug fix - 8.0.0 failed to setup system

--- lib/et_full_system/cli/local.rb
+++ lib/et_full_system/cli/local.rb
@@ -142,7 +142,7 @@ module EtFullSystem
     method_option :in_docker_compose, type: :boolean, default: false, desc: 'Set to true to assume certain services are in docker compose'
     def setup
       setup_depencencies
-      setup_ruby_versions unless !rvm_installed || options.in_docker_compose?
+      setup_ruby_versions unless !rvm_installed? || options.in_docker_compose?
       setup_services
     end
 
