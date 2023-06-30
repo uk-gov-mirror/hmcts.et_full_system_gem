53d42cf63e1a4ba0f7fc1fcc8d26d7a409528e83
garytaylor <garytaylor.code@gmail.com>
2023-06-30 12:54:21 +0100

    (RST-5516) Added gnupg2 to docker file

--- lib/et_full_system/cli/local.rb
+++ lib/et_full_system/cli/local.rb
@@ -509,7 +509,7 @@ module EtFullSystem
       return if rvm_installed?
 
       puts "------------------------------------------------ Installing rvm ---------------------------------------------------"
-      cmds = ["gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"]
+      cmds = ["gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"]
       cmds << "bash --login -c \"cd #{PROJECT_PATH} && curl -sSL https://get.rvm.io | bash -s stable\""
 
       cmds.each do |cmd|
--- lib/et_full_system/version.rb
+++ lib/et_full_system/version.rb
@@ -1,3 +1,3 @@
 module EtFullSystem
-  VERSION = "8.1.0-rc2"
+  VERSION = "8.1.0-rc3"
 end
