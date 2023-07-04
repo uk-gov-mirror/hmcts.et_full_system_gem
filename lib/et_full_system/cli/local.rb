7d41177a6e2a7a1aadebd521a2e24d4c75f3fd78
garytaylor <garytaylor.code@gmail.com>
2023-07-04 09:08:36 +0100

    (RST-5516) Removed disabling of autolibs

--- lib/et_full_system/cli/local.rb
+++ lib/et_full_system/cli/local.rb
@@ -511,7 +511,6 @@ module EtFullSystem
       puts "------------------------------------------------ Installing rvm ---------------------------------------------------"
       cmds = ["gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"]
       cmds << "bash --login -c \"cd #{PROJECT_PATH} && curl -sSL https://get.rvm.io | bash -s stable\""
-      cmds << "rvm autolibs disable"
 
       cmds.each do |cmd|
         puts cmd
--- lib/et_full_system/version.rb
+++ lib/et_full_system/version.rb
@@ -1,3 +1,3 @@
 module EtFullSystem
-  VERSION = "8.1.0-rc7"
+  VERSION = "8.1.0-rc8"
 end
